35429bc35e1afeae243cb58fcc7e7910ea2a07fd
KANHA <kanha.chhirolya13@gmail.com>
2022-09-13 23:22:28 -0400

    prime logic

--- prime.js
+++ prime.js
@@ -3,7 +3,7 @@ let i = 0; j = string.length-1;
 let value = "It is a palindrome"
 while(i<=j){
     if(string[i]!=string[j]){
-        console.log("It is not a palindrome");
+        console.log("It is prime number");
         break;
     }
 }
